8bcb04a8d2c94f07401e64a5e6ed05fa6b551046
Nathan Lafferty <lcgamer333@gmail.com>
2015-04-07 14:34:19 -0500

    Switch from xbee write to ser
    Switched Xbee write code to use the serial equivalent, don’t need extra
    xbee features

--- Src/MCUServer.py
+++ Src/MCUServer.py
@@ -9,11 +9,13 @@ import time
 #loops through to infinite, checking for input
 #if input is here, process it
 #input can come from user or any of the LCUs
+#LID OPEN
+#LID ACKN
+#LID BATT 6300
 
 def main():
 	server = MCUServer()
 	server.Run()
-
 
 class MCUServer:
 
@@ -24,16 +26,23 @@ class MCUServer:
 		dal.InitializeDB()
 
 
-		self.port = '/dev/tty.usbserial-A6007wOm'
-		#port = '/dev/ttyUSB0'
+		#self.port = '/dev/tty.usbserial-A6007wOm'
+		self.port = '/dev/ttyUSB0'
 		self.baud = 19200
+		self.ServerPanID = '3332'
+		self.Timeout = 5
 
 		print 'Port: ' + self.port
 		print 'Baud Rate: ' + str(self.baud)
 
 		try:
-			self.ser = serial.Serial(self.port, self.baud)
-			self.xbee = XBee(self.ser, callback=self.ReceiveData)
+			self.ser = serial.Serial(self.port, self.baud, timeout=self.Timeout)
+			self.xbee = XBee(self.ser)
+
+
+
+			self.xbee.send('at', command='ID', parameter=self.ServerPanID)
+			print 'Set XBee panID to: ' + self.ServerPanID
     
 		except Exception, e:
 			print 'Could not initialize serial interface.'
@@ -43,30 +52,48 @@ class MCUServer:
 		finally:
 			pass
 
-	def ReceiveData(self, data):
-		print data
-
 	def Run(self):	
 		xbeeDest = '\x00\x00'
-		receivedAck = False
 		shouldExit = False
 
 		while not shouldExit:
+
+			#initialize loop variables
+			receivedAckOrTimedout = False
+
 			txt = raw_input('Please swipe your ISU IdCard\n')
 
 			print 'Your card number is: ' + txt
-
 			print 'Sending message until we get an Ack or we timeout'
 
-			while not receivedAck:
-				self.xbee.send('tx', dest_addr=xbeeDest, data=txt)
+			#Grab the start time to determine if we have timedout on locker acking us
+			startTime = time.time()
+			print 'Checking Start Time: ' + str(startTime)
+
+			#While we have not received an ack, we haven't timed out, then continue to send
+			while not receivedAckOrTimedout:
+
+				#send data
+				self.ser.write(txt)
 				print 'Sent: ' + txt
 
-				try:
-					time.sleep(0.001)
-				except KeyboardInterrupt:
-					shouldExit = True
-					break;
+				#Is there any data waiting?
+				if(self.ser.inWaiting() > 0):
+					print self.ser.read()
+
+				#Have we timed out?
+				if((time.time() - startTime) > 10):
+					print 'TImeout Occured'
+					receivedAckOrTimedout = True
+				else:
+					try:
+						#sleep
+						time.sleep(0.333)
+					except KeyboardInterrupt:
+						receivedAckOrTimedout = True
+						shouldExit = True
+						break;
+
 
 		self.xbee.halt()
 		self.ser.close()
